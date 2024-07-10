fa06d91e0e264c6f825b6f3f4b939bd1c4458f96
Alejandro Dighiero <adighiero1@gmail.com>
2024-07-10 14:40:00 -0400

    portafolio

---
+++ src/data/user.js
@@ -0,0 +1,74 @@
+const INFO = {
+	main: {
+		title: "React Portafolio by Alejandro Dighiero",
+		name: "Alejandro Jose Dighiero Lanaspa.",
+		email: "adighiero1@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+	
+		github: "https://github.com/adighiero1",
+		linkedin: "https://www.linkedin.com/in/alejandrodighiero/",
+		instagram: "https://instagram.com/alejandrodighiero",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "Full-stack web developer.",
+		description:
+			"Hello! I am MERN fullstack web developer ",
+	},
+
+	about: {
+		title: "I’m Alejandro Dighiero. I Live in Montevideo Uruguay, where I spend my time learning new technologies and designing great dynamic websites!",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Links Uruguay",
+			description:
+				"Links Uruguay is a live webpage dedicated to offering traveling services all throughout the country of Uruguay.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://linksuru.vercel.app/",
+		},
+
+		{
+			title: "Rep Uruguay",
+			description:
+				"Rep Uruguay is a live web platform that connects spare parts buyers with spare parts dealers.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://rep.com.uy/",
+		},
+
+		{
+			title: "Instabook",
+			description:
+				"Instabook was a fun project where I created a social media website similar to facebook.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/adighiero1/True-Codes-Final-Project",
+		},
+
+		// {
+		// 	title: "Project 4",
+		// 	description:
+		// 		"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+		// 	logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+		// 	linkText: "View Project",
+		// 	link: "https://github.com",
+		// },
+	],
+};
+
+export default INFO;
---
+++ src/pages/projects.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect } from "react";
+import { Helmet } from "react-helmet";
+
+import NavBar from "../components/common/navBar";
+import Footer from "../components/common/footer";
+import Logo from "../components/common/logo";
+import AllProjects from "../components/projects/allProjects";
+
+import INFO from "../data/user";
+import SEO from "../data/seo";
+
+import "./styles/projects.css";
+
+const Projects = () => {
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, []);
+
+	const currentSEO = SEO.find((item) => item.page === "projects");
+
+	return (
+		<React.Fragment>
+			<Helmet>
+				<title>{`Projects | ${INFO.main.title}`}</title>
+				<meta name="description" content={currentSEO.description} />
+				<meta
+					name="keywords"
+					content={currentSEO.keywords.join(", ")}
+				/>
+			</Helmet>
+
+			<div className="page-content">
+				<NavBar active="projects" />
+				<div className="content-wrapper">
+					<div className="projects-logo-container">
+						<div className="projects-logo">
+							<Logo width={46} />
+						</div>
+					</div>
+					<div className="projects-container">
+						<div className="title projects-title">
+							Here is a sample of some projects I have worked on over the past couple of years as I learned new technologies.
+
+						</div>
+
+						<div className="subtitle projects-subtitle">
+							I've worked on a variety of projets over the past couple of years that include many differnt kinds of technologies. Bellow you
+							can explore a few of my noteworthy projects. You can also check out my github to see current projects I am working on.
+						</div>
+
+						<div className="projects-list">
+							<AllProjects />
+						</div>
+					</div>
+					<div className="page-footer">
+						<Footer />
+					</div>
+				</div>
+			</div>
+		</React.Fragment>
+	);
+};
+
+export default Projects;
